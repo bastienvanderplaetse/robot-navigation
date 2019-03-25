75e037df60a3330937cf62822be82b93daf63fc8
jbdel <jbdjbd001@gmail.com>
2019-03-25 11:03:33 +0100

    ok

--- nmtpytorch/layers/GAN/__init__.py
+++ nmtpytorch/layers/GAN/__init__.py
@@ -1,6 +1,5 @@
 from .discriminator import Discriminator
-from .discriminator_no import Discriminator_no
 from .generator import Generator
-from .generator_no import Generator_no
 from .rnn import RNN
 from .GRUCell import GRUCell
+from .LSTMCell import LSTMCell
--- nmtpytorch/models/__init__.py
+++ nmtpytorch/models/__init__.py
@@ -1,5 +1,4 @@
 from .GAN import GAN
 from .nmt import NMT
 from .WGAN import WGAN
-from .WGAN_NO import WGAN_NO
 
